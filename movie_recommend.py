9f2fec527643645354d7ceeca6a927d38cd029c5
ADI711 <adithyashankar10@gmail.com>
2023-03-09 23:57:35 +0000

    feat: intial commmit
    this is the inital implementation of the recommender system

---
+++ movie_recommend.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer 
+from sklearn.metrics.pairwise import cosine_similarity
+
+def get_title_from_index(index):
+    return df[df.index == index]['title'].values[0]
+
+def get_index_from_title(title):
+    return df[df.title == title]['index'].values[0]
+
+df = pd.read_csv("movies.csv")
+# print(df.columns)
+
+features = ['genres','overview','production_companies','cast','crew','director']
+for feature in features:
+    df[feature] = df[feature].fillna('')
+
+def combine_features(row):
+    try:
+        return row['genres']+" "+row['overview']+" "+row['production_companies']+" "+row['cast']+" "+row['crew']+" "+row['director']
+    except:
+        print ("error",row)
+
+df['cf'] = df.apply(combine_features,axis = 1)
+
+cv = CountVectorizer()
+count_matrix = cv.fit_transform(df['cf'])
+sim = cosine_similarity(count_matrix)
+
+fav_movie = "Inception"
+fav_movie_index = get_index_from_title(fav_movie)
+
+similar_movies = list(enumerate(sim[fav_movie_index]))
+# print (similar_movies)
+
+sorted_movies = sorted(similar_movies, key = lambda x:x[1], reverse = True)
+# print (sorted_movies)
+
+i = 0
+for movie in sorted_movies:
+    print (get_title_from_index(movie[0]))
+    i+=1
+    if i > 50:
+        break
